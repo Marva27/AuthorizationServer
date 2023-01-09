b68b3b90d545b7f13b53c476014aec0aca8cc571
Srinivasan Ramu <srinimarva@gmail.com>
2023-01-09 14:55:19 -0600

    Add files via upload

---
+++ src/test/java/com/authorizationserver/www/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authorizationserver.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
